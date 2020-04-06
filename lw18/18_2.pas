a0af93a7ddf99f78936c212e5e12e258872cc6a8
DenPetrov11032001 <den11032001@mail.ru>
2020-04-06 21:34:28 +0300

    FIX 18_2

--- lw18/18_2.pas
+++ lw18/18_2.pas
@@ -12,12 +12,14 @@ VAR
   NextScore: Score;
   Ave, TotalScore, ClassTotal: INTEGER; 
   Ch: CHAR;  
-  NameStudentFile: TEXT;  
+  NameStudentFile: TEXT; 
+  IsOverflow: BOOLEAN; 
 BEGIN {AverageScore}
   ClassTotal := 0;
   WRITELN('Student averages:');
   Student := 0;
-  WHILE Student < ClassSize
+ IsOverflow := FALSE;
+  WHILE (Student < ClassSize) AND (NOT IsOverflow)
   DO 
     BEGIN
       WRITELN(OUTPUT, 'Enter the student''s grade soname and number');
@@ -43,39 +45,51 @@ BEGIN {AverageScore}
         END;
       TotalScore := 0;
       WhichScore := 0;
-      WHILE WhichScore < ClassSize
+      WHILE (WhichScore < ClassSize)  AND (NOT IsOverflow)
       DO
         BEGIN
           IF NOT EOLN(INPUT)
           THEN
             READ(NextScore);
-          IF (NextScore < MinScore) OR (NextScore > MaxScore)
+          IsOverflow := (NextScore < MinScore) OR (NextScore > MaxScore);
+          IF NOT IsOverflow
           THEN
-            WRITELN(OUTPUT, 'NOT CORRECT DATA');
-          TotalScore := TotalScore + NextScore;
-          WhichScore := WhichScore + 1
+            BEGIN
+              TotalScore := TotalScore + NextScore;
+              WhichScore := WhichScore + 1
+            END
         END;
-      READLN;
-      TotalScore := TotalScore * 10;
-      Ave := TotalScore DIV NumberOfScores;
-      WRITE(OUTPUT, 'Average rating of the ');
-      RESET(NameStudentFile);
-      WHILE NOT EOLN(NameStudentFile)
-      DO
-        BEGIN
-          READ(NameStudentFile, Ch);
-          WRITE(OUTPUT, Ch)
-        END;
-      IF Ave MOD 10 >= 5
+      READLN(INPUT);
+      IF NOT IsOverflow
       THEN
-        WRITELN(OUTPUT, ': is ', Ave DIV 10 + 1)  
-      ELSE
-        WRITELN(OUTPUT, ': is ', Ave DIV 10);
-      ClassTotal := ClassTotal + TotalScore;
-      Student := Student + 1
+        BEGIN
+          TotalScore := TotalScore * 10;
+          Ave := TotalScore DIV NumberOfScores;
+          WRITE(OUTPUT, 'Average rating of the ');
+          RESET(NameStudentFile);
+          WHILE NOT EOLN(NameStudentFile)
+          DO
+            BEGIN
+              READ(NameStudentFile, Ch);
+              WRITE(OUTPUT, Ch)
+            END;
+          IF Ave MOD 10 >= 5
+          THEN
+            WRITELN(OUTPUT, ': is ', Ave DIV 10 + 1)  
+          ELSE
+            WRITELN(OUTPUT, ': is ', Ave DIV 10);
+          ClassTotal := ClassTotal + TotalScore;
+          Student := Student + 1
+        END;
     END;
-  WRITELN;
-  WRITELN(OUTPUT, 'Class average:');
-  ClassTotal := ClassTotal DIV ((ClassSize - 1) * NumberOfScores);
-  WRITELN(OUTPUT, ClassTotal DIV 10, '.',  ClassTotal MOD 10:1)
+  WRITELN;  
+  IF IsOverflow
+  THEN
+    WRITELN(OUTPUT, 'NOT CORRECT DATA')
+  ELSE
+    BEGIN  
+      WRITELN(OUTPUT, 'Class average:');
+      ClassTotal := ClassTotal DIV ((ClassSize - 1) * NumberOfScores);
+      WRITELN(OUTPUT, ClassTotal DIV 10, '.',  ClassTotal MOD 10:1)
+  END;
 END.  {AverageScore}
