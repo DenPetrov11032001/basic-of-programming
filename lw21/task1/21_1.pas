fea6ebb7885988da1bb3a36b2656adc5980e834c
DenPetrov11032001 <den11032001@mail.ru>
2020-04-29 16:45:18 +0300

    lw21

---
+++ lw21/task1/21_1.pas
@@ -0,0 +1,74 @@
+PROGRAM Encryption(INPUT, OUTPUT);
+CONST
+  Len = 20;
+TYPE
+  LengthStr = 1 .. Len;
+  Str = ARRAY [1 .. Len] OF ' ' .. 'Z';
+  Chiper = ARRAY [' ' .. 'Z'] OF CHAR;
+VAR
+  Msg: Str;
+  Code: Chiper;
+  I: INTEGER;
+
+PROCEDURE Initialize(VAR Code: Chiper);
+BEGIN  {Initialize}
+  Code[' '] := '#';
+  Code['A'] := 'Z';
+  Code['B'] := 'Y';
+  Code['C'] := 'X';
+  Code['D'] := '#';
+  Code['E'] := 'V';
+  Code['F'] := 'U';
+  Code['G'] := 'T';
+  Code['H'] := 'S';
+  Code['I'] := 'I';
+  Code['J'] := 'Q';
+  Code['K'] := 'P';
+  Code['L'] := '!';
+  Code['M'] := 'N';
+  Code['N'] := 'M';
+  Code['O'] := '2';
+  Code['P'] := 'K';
+  Code['Q'] := '$';
+  Code['R'] := 'D';
+  Code['S'] := 'H';
+  Code['T'] := '*';
+  Code['U'] := 'F';
+  Code['V'] := 'E';
+  Code['W'] := 'T';
+  Code['X'] := 'C';
+  Code['Y'] := 'B';
+  Code['Z'] := 'A';
+END;  {Initialize}
+ 
+PROCEDURE Encode(VAR Msg: Str; VAR Code: Chiper; VAR LenUserStr: INTEGER);
+VAR
+  Index: LengthStr;
+BEGIN  {Encode}
+  FOR Index := 1 TO LenUserStr
+  DO
+    BEGIN
+      WRITE(Code[Msg[Index]]);
+    END;
+  WRITELN
+END;  {Encode}
+
+ 
+BEGIN  {Encryption} 
+ Initialize(Code);
+ WHILE NOT EOF
+ DO
+   BEGIN
+     I := 0;
+     WHILE NOT EOLN AND (I < Len)
+     DO
+       BEGIN
+         I := I + 1;
+         READ(Msg[I]);
+         WRITE(Msg[I])
+       END;
+     READLN;
+     WRITELN;
+     Encode(Msg, Code, I)   
+   END 
+END.  {Encryption}
